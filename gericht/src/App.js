924b8c31b6180ac985dcb1955f7b138596988622
Muffinz1 <amiryou120@gmail.com>
2024-10-12 22:44:35 +0300

    App structure and arranging the components

--- gericht/src/App.js
+++ gericht/src/App.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import { Header } from './container';
-import {  Loader, OnlineReservations } from './components';
+import {  Loader, OnlineReservations,Navbar } from './components';
+import LoginPopup from './components/LoginPopup/LoginPopup';
 import './App.css';
 
 
 const App = () => {
+    const [showLogin, setShowLogin] = useState(false);
+    const [showReservations, setShowReservations] = useState(false);
+
+
     return (
-    <>
-    <Loader/>
-    <Header/>
-    </>
+        <>
+            {showLogin && <LoginPopup setShowLogin={setShowLogin} setShowReservations={setShowReservations} />}
+            <div className='app'>
+                <Loader />  
+                <Navbar setShowLogin={setShowLogin} />
+                <div className='content'>
+                    <Header />
+                </div>
+            </div>
+        </>
     );
 };
-
 export default App;
 
