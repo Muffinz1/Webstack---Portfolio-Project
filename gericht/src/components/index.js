720e19739ca82c60fd9034cd21efeffb0b95ce3b
Muffinz1 <amiryou120@gmail.com>
2024-10-12 22:45:26 +0300

    Exporting components to the app

--- gericht/src/components/index.js
+++ gericht/src/components/index.js
@@ -1,8 +1,10 @@
 import SubHeading from './SubHeading/SubHeading';
 import Loader from './Loader/Loader';
 import ReservationPopup from './BookingForm/OnlineReservations';
+import Navbar from './Navbar/Navbar';
 export {
   SubHeading,
   Loader,
-  ReservationPopup
+  ReservationPopup,
+  Navbar
 };
