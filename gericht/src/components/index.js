7f41d4b48ab31fdf086c0fc14f2876e3fece1531
Muffinz1 <amiryou120@gmail.com>
2024-10-14 03:38:05 +0300

    exporting of the MenuItem

--- gericht/src/components/index.js
+++ gericht/src/components/index.js
@@ -2,9 +2,11 @@ import SubHeading from './SubHeading/SubHeading';
 import Loader from './Loader/Loader';
 import ReservationPopup from './BookingForm/OnlineReservations';
 import Navbar from './Navbar/Navbar';
+import MenuItem from './Menuitem/MenuItem';
 export {
   SubHeading,
   Loader,
   ReservationPopup,
-  Navbar
+  Navbar,
+  MenuItem,
 };
