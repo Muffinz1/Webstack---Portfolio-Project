d77441446e548c85d5b33a4f0cea8c601e29778a
Muffinz1 <amiryou120@gmail.com>
2024-10-15 01:38:16 +0300

    Exporting containers to be utilized in the App Routing

--- gericht/src/container/index.js
+++ gericht/src/container/index.js
@@ -1,8 +1,13 @@
 import Header from './Header/Header';
 import AboutUs from './AboutUs/AboutUs'
 import SpecialMenu from './Menu/SpecialMenu'
+import Laurels from './Laurels/Laurels';
+import Chef from './Chef/Chef';
+
 export {
   Header,
   AboutUs,
   SpecialMenu,
+  Chef,
+  Laurels
 };
