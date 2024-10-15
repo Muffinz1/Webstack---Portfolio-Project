4b084fd94ec7f7f0f376ee3ad0faf48c27dea987
Muffinz1 <amiryou120@gmail.com>
2024-10-16 00:14:34 +0300

    exporting containers to the app

--- gericht/src/container/index.js
+++ gericht/src/container/index.js
@@ -3,7 +3,9 @@ import AboutUs from './AboutUs/AboutUs'
 import SpecialMenu from './Menu/SpecialMenu'
 import Laurels from './Laurels/Laurels';
 import Chef from './Chef/Chef';
-import Intro from './Intro/Intro'
+import Intro from './Intro/Intro';
+import Gallery from './Gallery/Gallery';
+import FindUs from './Findus/FindUs';
 
 export {
   Header,
@@ -11,5 +13,7 @@ export {
   SpecialMenu,
   Chef,
   Laurels,
-  Intro
+  Intro,
+  Gallery,
+  FindUs,
 };
