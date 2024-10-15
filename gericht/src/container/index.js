20407463c9814b7b9a3b527b3dd56bb9a20b57d8
Muffinz1 <amiryou120@gmail.com>
2024-10-15 10:17:38 +0300

    Exporting the intro component so we can use it in app file

--- gericht/src/container/index.js
+++ gericht/src/container/index.js
@@ -3,11 +3,13 @@ import AboutUs from './AboutUs/AboutUs'
 import SpecialMenu from './Menu/SpecialMenu'
 import Laurels from './Laurels/Laurels';
 import Chef from './Chef/Chef';
+import Intro from './Intro/Intro'
 
 export {
   Header,
   AboutUs,
   SpecialMenu,
   Chef,
-  Laurels
+  Laurels,
+  Intro
 };
