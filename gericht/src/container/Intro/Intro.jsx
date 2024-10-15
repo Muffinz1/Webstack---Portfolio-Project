d7dfcd7a25f0399ad6ee0a884b9c0d790b5cfc00
Muffinz1 <amiryou120@gmail.com>
2024-10-15 10:17:10 +0300

    Adding the video intro component

---
+++ gericht/src/container/Intro/Intro.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useRef } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
+
+import './Intro.css';
+
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(true);
+  const vidRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => {
+      if (prevPlayVideo) {
+        vidRef.current.pause();
+      } else {
+        vidRef.current.play();
+      }
+      return !prevPlayVideo;
+    });
+  };
+
+  return (
+    <div
+      className="app__video"
+      onMouseEnter={() => setShowOverlay(true)}   
+      onMouseLeave={() => setShowOverlay(false)}  
+    >
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      {showOverlay && (
+        <div className="app__video-overlay flex__center">
+          <div
+            className="app__video-overlay_circle flex__center"
+            onClick={handleVideo}
+          >
+            {playVideo ? (
+              <BsPauseFill color="#fff" fontSize={30} />
+            ) : (
+              <BsFillPlayFill color="#fff" fontSize={30} />
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Intro;
