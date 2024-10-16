90c8ff20c543f9a0a43c08752866c6712bb0e384
Muffinz1 <amiryou120@gmail.com>
2024-10-17 02:27:21 +0300

    export band component to app

--- gericht/src/constants/index.js
+++ gericht/src/constants/index.js
@@ -1,5 +1,6 @@
 import images from './images';
 import meal from '../assets/meal.mp4';
 import data from './data';
+import music from '../assets/music.mp3'
 
-export { images, meal, data };
+export { images, meal, data, music };
