1f828b761c25dd3af5e86e97a2b79ecd8dac7c96
Muffinz1 <amiryou120@gmail.com>
2024-10-17 02:25:22 +0300

    exporting Band Component

--- gericht/src/components/index.js
+++ gericht/src/components/index.js
@@ -3,10 +3,12 @@ import Loader from './Loader/Loader';
 import ReservationPopup from './BookingForm/OnlineReservations';
 import Navbar from './Navbar/Navbar';
 import MenuItem from './Menuitem/MenuItem';
+import Band from './Band/Band'
 export {
   SubHeading,
   Loader,
   ReservationPopup,
   Navbar,
   MenuItem,
+  Band,
 };
